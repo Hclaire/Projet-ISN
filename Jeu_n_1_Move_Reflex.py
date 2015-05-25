70763779ff7d05347572a72e67b13a178af00f82
Huang Claire <mp.claire97@gmail.com>
2015-05-25 10:17:38 +0200

    commit du matin edit du 24.05.15

--- Jeu_n_1_Move_Reflex.py
+++ Jeu_n_1_Move_Reflex.py
@@ -19,12 +19,27 @@ from pygame.locals import *
 pygame.init() #Ouverture de la fen锚tre Pygame
 
 fenetre = pygame.display.set_mode((1150, 700))  
+defaite_gauche = 0
+defaite_droite = 0
 fond_accueil = pygame.image.load("./fond/fond1.png").convert_alpha()
 image_regle_jeu = pygame.image.load("./fond/reglejeu.png").convert_alpha()
 fenetre.blit(fond_accueil, (0,0))
 pygame.display.flip() #Rafra卯chissement de l'茅cran
 pygame.time.wait(4000)
+fond_general = pygame.image.load("./fond/fond.png").convert_alpha()
+fond_J1 = pygame.image.load("./fond/fond_panneau.png").convert_alpha()
+fond_J2 = pygame.image.load("./fond/fond_panneau.png").convert_alpha()
+    
 
+
+fenetre.blit(fond_general, (0,0))
+fenetre.blit (fond_J1, (0,200))
+fenetre.blit (fond_J2, (600,200))
+
+pygame.display.flip() #Rafra卯chissement de l'茅cran
+
+
+"""
 i = 0
 while i < 50:
     
@@ -35,21 +50,11 @@ while i < 50:
     image_regle_jeu = pygame.image.load("./fond/reglejeu.png").convert_alpha()
     fenetre.blit(image_regle_jeu, (0,0))
     pygame.display.flip()
-
+    pygame.time.wait(4000)
+    
     i += 1
-               
-def choix_fond(fenetre):
-
-    fond_general = pygame.image.load("./fond/fond.png").convert_alpha()
-    fond_J1 = pygame.image.load("./fond/fond_panneau.png").convert_alpha()
-    fond_J2 = pygame.image.load("./fond/fond_panneau.png").convert_alpha()
     
-
-
-    fenetre.blit(fond_general, (0,0))
-    fenetre.blit (fond_J1, (0,200))
-    fenetre.blit (fond_J2, (600,200))
-    
+"""            
 
 def choix_fleches(numero, fenetre, abscisse, ordonnee):
 
@@ -89,11 +94,15 @@ def choix_fleches(numero, fenetre, abscisse, ordonnee):
     fenetre.blit(fleche,(abscisse, ordonnee))
 
     
-def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
+def evenement(defaite_gauche, defaite_droite, numero_fleches_J1, numero_fleches_J2, fenetre):
     
     continuer = True
     son_correct = pygame.mixer.Sound("./Son_touches/son_correct.wav")
     son_incorrect = pygame.mixer.Sound("./Son_touches/son_incorrect.wav")
+    compteur = 0
+    
+    image_perdu = pygame.image.load("./fond/fond_panneau_perdu.png").convert_alpha()
+    image_gagne = pygame.image.load("./fond/fond_panneau_gagne.png").convert_alpha()
     while continuer:
         
         for event in pygame.event.get():    #Attente des 茅v茅nements
@@ -102,7 +111,7 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
                 
                 continuer = False
             
-            elif event.type == KEYDOWN :
+            elif event.type == KEYDOWN and compteur < 1 :
                     
                 if event.key == K_d: #Si "fl猫che bas"
                         
@@ -110,14 +119,16 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
 
                         result = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()
-
+                        k_J1 = 1
                     else:
 
                         result = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-
+                        k_J1 = 0
+                    compteur +=1
 
                     fenetre.blit(result, (160,300))
+                    
 
                 elif event.key == K_s:
 
@@ -125,13 +136,14 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
 
                         result = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()
-                        k = 1
+                        k_J1 = 1
             
                     else:
 
                         result = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-                        k = 0
+                        k_J1 = 0
+                    compteur +=1
                     fenetre.blit(result, (160,300))
 
                 elif event.key == K_e: 
@@ -140,12 +152,12 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
                             
                         result = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()       
-                        k = 1
+                        k_J1 = 1
                     else:
                             
                         result = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-                        k = 0
+                        k_J1 = 0
                     fenetre.blit(result, (160,300))
 
                 elif event.key == K_f:
@@ -154,27 +166,46 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
                             
                         result = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()
-                        k = 1
+                        k_J1 = 1
                     else:
 
                         result = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()                   
-                        k = 0
+                        k_J1 = 0
+                    compteur +=1
                     fenetre.blit(result, (160,300))
+                else:
+                    k_J1 = 3
 
+                pygame.display.flip()
+                if k_J1 == 0:
+                    defaite_gauche += 1
+                    if defaite_gauche == 1:
+                        deux_vies = pygame.image.load("./fond/fond_panneau_2vies.png").convert_alpha()
 
-                elif event.key == K_DOWN: #Si "fl猫che bas"
+                        fenetre.blit (deux_vies, (0,200))
+                    elif defaite_gauche == 2:
+                        une_vie = pygame.image.load("./fond/fond_panneau_1vie.png").convert_alpha()
+                        fenetre.blit(une_vie,(0,200))
+                    else:
+                        
+                        fenetre.blit(image_perdu,(0,200))
+                    pygame.display.flip()
+                    pygame.time.wait(2000)
+
+                if event.key == K_DOWN: #Si "fl猫che bas"
                         
                     if numero_fleches_J2 == 1 or numero_fleches_J2 == 7:
                             
                         result1 = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()  
-                        k = 1
+                        k_J2 = 1
                     else:
 
                         result1 = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-                        k = 0
+                        k_J2 = 0
+                    compteur +=1
                     fenetre.blit(result1, (750,300))
                     
                 elif event.key == K_RIGHT:
@@ -183,12 +214,13 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
                             
                         result1 = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()
-                        k = 1
+                        k_J2 = 1
                     else:
 
                         result1 = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-                        k = 0
+                        k_J2 = 0
+                    compteur +=1
                     fenetre.blit(result1, (750,300))
                     
                 elif event.key == K_LEFT: 
@@ -197,12 +229,13 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
                             
                         result1 = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play() 
-                        k = 1
+                        k_J2 = 1
                     else:
 
                         result1 = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-                        k = 0
+                        k_J2 = 0
+                    compteur +=1
                     fenetre.blit(result1, (750,300))
                     
                 elif event.key == K_UP:
@@ -211,94 +244,59 @@ def evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies):
                             
                         result1 = pygame.image.load("./correct-incorrect/correct.png").convert_alpha()
                         son_correct.play()
-                        k = 1
+                        k_J2 = 1
                     else:
 
                         result1 = pygame.image.load("./correct-incorrect/incorrect.png").convert_alpha()
                         son_incorrect.play()
-                        k = 0
+                        k_J2 = 0
+                    compteur +=1
                     fenetre.blit(result1, (750,300))
-            
-                   
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
+                else :
+                    k_J2 = 3
+    
+ 
+                if k_J2 == 0 :
+                    
+                    defaite_droite += 1
+                    if defaite_droite == 1:
+                        deux_vies = pygame.image.load("./fond/fond_panneau_2vies.png").convert_alpha()
+                        fenetre.blit (deux_vies, (600,200))
+                    elif defaite_droite == 2:
+                        une_vie = pygame.image.load("./fond/fond_panneau_1vie.png").convert_alpha()
+                        fenetre.blit(une_vie,(600,200))
+                    else:
+                        fenetre.blit(image_perdu,(600,200))
+                        
+                pygame.display.flip()
                 pygame.time.wait(2000)
                 
-            if k == 1:
+         
+                while (defaite_gauche<4 or defaite_droite<4):
+                    defaite_gauche, defaite_droite = main_01(defaite_gauche, defaite_droite)
                 
-                abscisse_fleche_J1 = 0
-                ordonnee_fleche_J1 = 200
-    
-                abscisse_fleche_J2 = 600
-                ordonnee_fleche_J2 = 200
-
-                choix_fond(fenetre)
-
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
-
-                numero_fleches_J1 = random.randint(1,8)
-                numero_fleches_J2 = random.randint(1,8)
-    
-                choix_fleches(numero_fleches_J1, fenetre, abscisse_fleche_J1, ordonnee_fleche_J1)
-                choix_fleches(numero_fleches_J2, fenetre,abscisse_fleche_J2, ordonnee_fleche_J2  )
-
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
-
-                evenement(numero_fleches_J1, numero_fleches_J2, fenetre)
-
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
-            elif k == 0:
-                abscisse_fleche_J1 = 0
-                ordonnee_fleche_J1 = 200
-    
-                abscisse_fleche_J2 = 600
-                ordonnee_fleche_J2 = 200
-
-                choix_fond(fenetre)
-
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
-
-                numero_fleches_J1 = random.randint(1,8)
-                numero_fleches_J2 = random.randint(1,8)
-    
-                choix_fleches(numero_fleches_J1, fenetre, abscisse_fleche_J1, ordonnee_fleche_J1)
-                choix_fleches(numero_fleches_J2, fenetre,abscisse_fleche_J2, ordonnee_fleche_J2  )
-
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
-
-                evenement(numero_fleches_J1, numero_fleches_J2, fenetre, vies-1)
-
-                pygame.display.flip() #Rafra卯chissement de l'茅cran
     
     pygame.quit()
+    return defaite_gauche, defaite_droite
+def main_01(defaite_gauche, defaite_droite):
     
-def main_01():
+
+    abscisse_fleche_J1 = 0
+    ordonnee_fleche_J1 = 200
     
-    continuer = 1
-    while continuer:
-        for event in pygame.event.get():    #Attente des 茅v茅nements
-            if event.type == QUIT:
-                continuer = 0
-            elif event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:    #Si clic gauche
-                    abscisse_fleche_J1 = 0
-                    ordonnee_fleche_J1 = 200
+    abscisse_fleche_J2 = 600
+    ordonnee_fleche_J2 = 200
+
+
+    numero_fleches_J1 = random.randint(1,8)
+    numero_fleches_J2 = random.randint(1,8)
     
-                    abscisse_fleche_J2 = 600
-                    ordonnee_fleche_J2 = 200
+    choix_fleches(numero_fleches_J1, fenetre, abscisse_fleche_J1, ordonnee_fleche_J1)
+    choix_fleches(numero_fleches_J2, fenetre,abscisse_fleche_J2, ordonnee_fleche_J2  )
 
-                    choix_fond(fenetre)
+    pygame.display.flip() #Rafra卯chissement de l'茅cran
 
-                    pygame.display.flip() #Rafra卯chissement de l'茅cran
+    defaite_gauche, defaite_droite = evenement(defaite_gauche, defaite_droite, numero_fleches_J1, numero_fleches_J2, fenetre)
 
-                    numero_fleches_J1 = random.randint(1,8)
-                    numero_fleches_J2 = random.randint(1,8)
-    
-                    choix_fleches(numero_fleches_J1, fenetre, abscisse_fleche_J1, ordonnee_fleche_J1)
-                    choix_fleches(numero_fleches_J2, fenetre,abscisse_fleche_J2, ordonnee_fleche_J2  )
-
-                    pygame.display.flip() #Rafra卯chissement de l'茅cran
-
-                    evenement(numero_fleches_J1, numero_fleches_J2, fenetre)
-
-                    pygame.display.flip() #Rafra卯chissement de l'茅cran
-main_01()
+    pygame.display.flip() #Rafra卯chissement de l'茅cran
+main_01(defaite_gauche, defaite_droite)
--- fenetre_principale.py
+++ fenetre_principale.py
@@ -9,10 +9,3 @@ from pygame.locals import *
 
 
 from ProjetV1_7_23_05_15 import main_02
-
-defaite_gauche = 0
-defaite_droite = 0
-while defaite_gauche <4 or defaite_droite <4:
-    defaite_gauche, defaite_droite = main_02(defaite_gauche,defaite_droite)
-    pygame.time.wait(2000)
-       
